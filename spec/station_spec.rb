4be34c91d5abe25bfcae72d5b2340b453ced88db
Nikita <latinisnotdead07@gmail.com>
2020-05-20 15:17:47 +0100

    create station_info instance hash

---
+++ spec/station_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../lib/station'
+
+describe Station do
+  let(:station) { Station.new("Waterloo", 1) }
+
+  it 'should have a name' do
+    expect(station.station_info[:name]).to eq("Waterloo")
+  end
+
+  it 'should have a zone' do
+    expect(station.station_info[:zone]).to eq(1)
+  end
+end
